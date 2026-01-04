4cb2fdabc66d16ad3f78f9fb53d12b40f762e3f4
harishwarrior <warriorharish95668@gmail.com>
2026-01-04 13:47:02 +0530

    feat: add delayed drag start to photo thumbnails

--- lib/src/features/home/presentation/widgets/photo_thumbnail.dart
+++ lib/src/features/home/presentation/widgets/photo_thumbnail.dart
@@ -17,7 +17,7 @@ class PhotoThumbnail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ReorderableDragStartListener(
+    return ReorderableDelayedDragStartListener(
       index: index,
       child: Container(
         width: 80,
